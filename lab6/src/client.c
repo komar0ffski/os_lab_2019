a7253f309f8dacb9749823d8464d38bd6ba00978
komar0ffski <komar0ffski@mail.ru>
2025-04-18 23:06:34 +0000

    Выполнена шестая лабораторная работа

--- lab6/src/client.c
+++ lab6/src/client.c
@@ -1,164 +1,162 @@
+#include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
-#include <errno.h>
 #include <getopt.h>
-#include <netdb.h>
 #include <netinet/in.h>
-#include <netinet/ip.h>
+#include <pthread.h>
 #include <sys/socket.h>
-#include <sys/types.h>
+#include <arpa/inet.h>
+#include "utils.h"
 
-struct Server {
-  char ip[255];
-  int port;
+// структура для хранения информации о сервере
+struct server {
+    char ip[64];
+    int port;
 };
 
-uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod) {
-  uint64_t result = 0;
-  a = a % mod;
-  while (b > 0) {
-    if (b % 2 == 1)
-      result = (result + a) % mod;
-    a = (a * 2) % mod;
-    b /= 2;
-  }
+// чтение списка серверов из файла
+int read_servers(const char *path, struct server **servers) {
+    FILE *file = fopen(path, "r");
+    if (file == NULL) {
+        fprintf(stderr, "ошибка: не удалось открыть файл %s\n", path);
+        return -1;
+    }
 
-  return result % mod;
+    size_t count = 0;
+    size_t capacity = 4;
+    *servers = malloc(capacity * sizeof(struct server));
+
+    // чтение ip и порта каждого сервера из файла
+    while (fscanf(file, "%63s %d", (*servers)[count].ip, &(*servers)[count].port) == 2) {
+        count++;
+        if (count >= capacity) {
+            capacity *= 2;
+            *servers = realloc(*servers, capacity * sizeof(struct server));
+        }
+    }
+
+    fclose(file);
+    return count;
 }
 
-bool ConvertStringToUI64(const char *str, uint64_t *val) {
-  char *end = NULL;
-  unsigned long long i = strtoull(str, &end, 10);
-  if (errno == ERANGE) {
-    fprintf(stderr, "Out of uint64_t range: %s\n", str);
-    return false;
-  }
+// отправка задачи на сервер и получение результата
+int send_task(const struct server *server, uint64_t begin, uint64_t end, uint64_t mod, uint64_t *result) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        fprintf(stderr, "ошибка: не удалось создать сокет\n");
+        return -1;
+    }
 
-  if (errno != 0)
-    return false;
+    // настройка адреса сервера
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(server->port);
+    inet_pton(AF_INET, server->ip, &server_addr.sin_addr);
 
-  *val = i;
-  return true;
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        fprintf(stderr, "ошибка: не удалось подключиться к серверу %s:%d\n", server->ip, server->port);
+        close(sock);
+        return -1;
+    }
+
+    // отправка задачи на сервер
+    uint64_t task[3] = {begin, end, mod};
+    if (send(sock, task, sizeof(task), 0) < 0) {
+        fprintf(stderr, "ошибка: не удалось отправить данные на сервер\n");
+        close(sock);
+        return -1;
+    }
+
+    // получение результата от сервера
+    if (recv(sock, result, sizeof(*result), 0) < 0) {
+        fprintf(stderr, "ошибка: не удалось получить данные от сервера\n");
+        close(sock);
+        return -1;
+    }
+
+    close(sock);
+    return 0;
 }
 
 int main(int argc, char **argv) {
-  uint64_t k = -1;
-  uint64_t mod = -1;
-  char servers[255] = {'\0'}; // TODO: explain why 255
+    uint64_t k = 0;
+    uint64_t mod = 0;
+    char *servers_path = NULL;
 
-  while (true) {
-    int current_optind = optind ? optind : 1;
+    // разбор аргументов командной строки
+    while (true) {
+        int current_optind = optind ? optind : 1;
 
-    static struct option options[] = {{"k", required_argument, 0, 0},
-                                      {"mod", required_argument, 0, 0},
-                                      {"servers", required_argument, 0, 0},
-                                      {0, 0, 0, 0}};
+        static struct option options[] = {
+            {"k", required_argument, 0, 'k'},
+            {"mod", required_argument, 0, 'm'},
+            {"servers", required_argument, 0, 's'},
+            {0, 0, 0, 0}};
 
-    int option_index = 0;
-    int c = getopt_long(argc, argv, "", options, &option_index);
+        int option_index = 0;
+        int c = getopt_long(argc, argv, "", options, &option_index);
 
-    if (c == -1)
-      break;
+        if (c == -1)
+            break;
 
-    switch (c) {
-    case 0: {
-      switch (option_index) {
-      case 0:
-        ConvertStringToUI64(optarg, &k);
-        // TODO: your code here
-        break;
-      case 1:
-        ConvertStringToUI64(optarg, &mod);
-        // TODO: your code here
-        break;
-      case 2:
-        // TODO: your code here
-        memcpy(servers, optarg, strlen(optarg));
-        break;
-      default:
-        printf("Index %d is out of options\n", option_index);
-      }
-    } break;
-
-    case '?':
-      printf("Arguments error\n");
-      break;
-    default:
-      fprintf(stderr, "getopt returned character code 0%o?\n", c);
-    }
-  }
-
-  if (k == -1 || mod == -1 || !strlen(servers)) {
-    fprintf(stderr, "Using: %s --k 1000 --mod 5 --servers /path/to/file\n",
-            argv[0]);
-    return 1;
-  }
-
-  // TODO: for one server here, rewrite with servers from file
-  unsigned int servers_num = 1;
-  struct Server *to = malloc(sizeof(struct Server) * servers_num);
-  // TODO: delete this and parallel work between servers
-  to[0].port = 20001;
-  memcpy(to[0].ip, "127.0.0.1", sizeof("127.0.0.1"));
-
-  // TODO: work continiously, rewrite to make parallel
-  for (int i = 0; i < servers_num; i++) {
-    struct hostent *hostname = gethostbyname(to[i].ip);
-    if (hostname == NULL) {
-      fprintf(stderr, "gethostbyname failed with %s\n", to[i].ip);
-      exit(1);
+        switch (c) {
+        case 'k':
+            k = strtoull(optarg, NULL, 10);
+            break;
+        case 'm':
+            mod = strtoull(optarg, NULL, 10);
+            break;
+        case 's':
+            servers_path = optarg;
+            break;
+        default:
+            fprintf(stderr, "использование: %s --k <значение> --mod <значение> --servers <путь>\n", argv[0]);
+            return 1;
+        }
     }
 
-    struct sockaddr_in server;
-    server.sin_family = AF_INET;
-    server.sin_port = htons(to[i].port);
-    server.sin_addr.s_addr = *((unsigned long *)hostname->h_addr);
-
-    int sck = socket(AF_INET, SOCK_STREAM, 0);
-    if (sck < 0) {
-      fprintf(stderr, "Socket creation failed!\n");
-      exit(1);
+    // проверка обязательных аргументов
+    if (k == 0 || mod == 0 || servers_path == NULL) {
+        fprintf(stderr, "использование: %s --k <значение> --mod <значение> --servers <путь>\n", argv[0]);
+        return 1;
     }
 
-    if (connect(sck, (struct sockaddr *)&server, sizeof(server)) < 0) {
-      fprintf(stderr, "Connection failed\n");
-      exit(1);
+    // загрузка списка серверов
+    struct server *servers = NULL;
+    int servers_count = read_servers(servers_path, &servers);
+    if (servers_count <= 0) {
+        fprintf(stderr, "ошибка: нет доступных серверов\n");
+        return 1;
     }
 
-    // TODO: for one server
-    // parallel between servers
-    uint64_t begin = 1;
-    uint64_t end = k;
+    // распределение работы между серверами
+    uint64_t chunk_size = k / servers_count;
+    uint64_t remaining = k % servers_count;
 
-    char task[sizeof(uint64_t) * 3];
-    memcpy(task, &begin, sizeof(uint64_t));
-    memcpy(task + sizeof(uint64_t), &end, sizeof(uint64_t));
-    memcpy(task + 2 * sizeof(uint64_t), &mod, sizeof(uint64_t));
+    uint64_t total = 1;
+    for (int i = 0; i < servers_count; i++) {
+        uint64_t begin = i * chunk_size + 1;
+        uint64_t end = (i + 1) * chunk_size;
 
-    if (send(sck, task, sizeof(task), 0) < 0) {
-      fprintf(stderr, "Send failed\n");
-      exit(1);
+        // добавление остатка к последнему серверу
+        if (i == servers_count - 1)
+            end += remaining;
+
+        uint64_t result = 0;
+        if (send_task(&servers[i], begin, end, mod, &result) < 0) {
+            fprintf(stderr, "ошибка: задача не выполнена на сервере %s:%d\n", servers[i].ip, servers[i].port);
+            free(servers);
+            return 1;
+        }
+
+        total = MultModulo(total, result, mod);
     }
 
-    char response[sizeof(uint64_t)];
-    if (recv(sck, response, sizeof(response), 0) < 0) {
-      fprintf(stderr, "Recieve failed\n");
-      exit(1);
-    }
+    printf("итоговый результат: %lu\n", total);
 
-    // TODO: from one server
-    // unite results
-    uint64_t answer = 0;
-    memcpy(&answer, response, sizeof(uint64_t));
-    printf("answer: %llu\n", answer);
-
-    close(sck);
-  }
-  free(to);
-
-  return 0;
+    free(servers);
+    return 0;
 }
--- lab6/src/server.c
+++ lab6/src/server.c
@@ -1,196 +1,161 @@
+// server.c
 #include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
 #include <getopt.h>
 #include <netinet/in.h>
-#include <netinet/ip.h>
+#include <pthread.h>
 #include <sys/socket.h>
-#include <sys/types.h>
+#include "utils.h"
 
-#include "pthread.h"
-
-struct FactorialArgs {
-  uint64_t begin;
-  uint64_t end;
-  uint64_t mod;
+// аргументы для вычисления факториала
+struct factorial_args {
+    uint64_t begin;
+    uint64_t end;
+    uint64_t mod;
 };
 
-uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod) {
-  uint64_t result = 0;
-  a = a % mod;
-  while (b > 0) {
-    if (b % 2 == 1)
-      result = (result + a) % mod;
-    a = (a * 2) % mod;
-    b /= 2;
-  }
-
-  return result % mod;
+// вычисление факториала в заданном диапазоне
+uint64_t calculate_factorial(const struct factorial_args *args) {
+    uint64_t result = 1;
+    for (uint64_t i = args->begin; i <= args->end; i++) {
+        result = MultModulo(result, i, args->mod);
+    }
+    return result;
 }
 
-uint64_t Factorial(const struct FactorialArgs *args) {
-  uint64_t ans = 1;
-
-  // TODO: your code here
-
-  return ans;
-}
-
-void *ThreadFactorial(void *args) {
-  struct FactorialArgs *fargs = (struct FactorialArgs *)args;
-  return (void *)(uint64_t *)Factorial(fargs);
+// функция для выполнения в потоке
+void *thread_factorial(void *args) {
+    struct factorial_args *fargs = (struct factorial_args *)args;
+    uint64_t *result = malloc(sizeof(uint64_t));
+    *result = calculate_factorial(fargs);
+    return result;
 }
 
 int main(int argc, char **argv) {
-  int tnum = -1;
-  int port = -1;
+    int tnum = -1;
+    int port = -1;
 
-  while (true) {
-    int current_optind = optind ? optind : 1;
-
-    static struct option options[] = {{"port", required_argument, 0, 0},
-                                      {"tnum", required_argument, 0, 0},
-                                      {0, 0, 0, 0}};
-
-    int option_index = 0;
-    int c = getopt_long(argc, argv, "", options, &option_index);
-
-    if (c == -1)
-      break;
-
-    switch (c) {
-    case 0: {
-      switch (option_index) {
-      case 0:
-        port = atoi(optarg);
-        // TODO: your code here
-        break;
-      case 1:
-        tnum = atoi(optarg);
-        // TODO: your code here
-        break;
-      default:
-        printf("Index %d is out of options\n", option_index);
-      }
-    } break;
-
-    case '?':
-      printf("Unknown argument\n");
-      break;
-    default:
-      fprintf(stderr, "getopt returned character code 0%o?\n", c);
-    }
-  }
-
-  if (port == -1 || tnum == -1) {
-    fprintf(stderr, "Using: %s --port 20001 --tnum 4\n", argv[0]);
-    return 1;
-  }
-
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-    fprintf(stderr, "Can not create server socket!");
-    return 1;
-  }
-
-  struct sockaddr_in server;
-  server.sin_family = AF_INET;
-  server.sin_port = htons((uint16_t)port);
-  server.sin_addr.s_addr = htonl(INADDR_ANY);
-
-  int opt_val = 1;
-  setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof(opt_val));
-
-  int err = bind(server_fd, (struct sockaddr *)&server, sizeof(server));
-  if (err < 0) {
-    fprintf(stderr, "Can not bind to socket!");
-    return 1;
-  }
-
-  err = listen(server_fd, 128);
-  if (err < 0) {
-    fprintf(stderr, "Could not listen on socket\n");
-    return 1;
-  }
-
-  printf("Server listening at %d\n", port);
-
-  while (true) {
-    struct sockaddr_in client;
-    socklen_t client_len = sizeof(client);
-    int client_fd = accept(server_fd, (struct sockaddr *)&client, &client_len);
-
-    if (client_fd < 0) {
-      fprintf(stderr, "Could not establish new connection\n");
-      continue;
-    }
-
+    // разбор аргументов командной строки
     while (true) {
-      unsigned int buffer_size = sizeof(uint64_t) * 3;
-      char from_client[buffer_size];
-      int read = recv(client_fd, from_client, buffer_size, 0);
+        static struct option options[] = {
+            {"port", required_argument, 0, 0},
+            {"tnum", required_argument, 0, 0},
+            {0, 0, 0, 0}};
 
-      if (!read)
-        break;
-      if (read < 0) {
-        fprintf(stderr, "Client read failed\n");
-        break;
-      }
-      if (read < buffer_size) {
-        fprintf(stderr, "Client send wrong data format\n");
-        break;
-      }
+        int option_index = 0;
+        int c = getopt_long(argc, argv, "", options, &option_index);
 
-      pthread_t threads[tnum];
+        if (c == -1)
+            break;
 
-      uint64_t begin = 0;
-      uint64_t end = 0;
-      uint64_t mod = 0;
-      memcpy(&begin, from_client, sizeof(uint64_t));
-      memcpy(&end, from_client + sizeof(uint64_t), sizeof(uint64_t));
-      memcpy(&mod, from_client + 2 * sizeof(uint64_t), sizeof(uint64_t));
-
-      fprintf(stdout, "Receive: %llu %llu %llu\n", begin, end, mod);
-
-      struct FactorialArgs args[tnum];
-      for (uint32_t i = 0; i < tnum; i++) {
-        // TODO: parallel somehow
-        args[i].begin = 1;
-        args[i].end = 1;
-        args[i].mod = mod;
-
-        if (pthread_create(&threads[i], NULL, ThreadFactorial,
-                           (void *)&args[i])) {
-          printf("Error: pthread_create failed!\n");
-          return 1;
+        switch (c) {
+        case 0:
+            if (strcmp(options[option_index].name, "port") == 0) {
+                port = atoi(optarg);
+            } else if (strcmp(options[option_index].name, "tnum") == 0) {
+                tnum = atoi(optarg);
+            }
+            break;
+        default:
+            fprintf(stderr, "неизвестный аргумент\n");
+            return 1;
         }
-      }
-
-      uint64_t total = 1;
-      for (uint32_t i = 0; i < tnum; i++) {
-        uint64_t result = 0;
-        pthread_join(threads[i], (void **)&result);
-        total = MultModulo(total, result, mod);
-      }
-
-      printf("Total: %llu\n", total);
-
-      char buffer[sizeof(total)];
-      memcpy(buffer, &total, sizeof(total));
-      err = send(client_fd, buffer, sizeof(total), 0);
-      if (err < 0) {
-        fprintf(stderr, "Can't send data to client\n");
-        break;
-      }
     }
 
-    shutdown(client_fd, SHUT_RDWR);
-    close(client_fd);
-  }
+    // проверка обязательных аргументов
+    if (port == -1 || tnum == -1) {
+        fprintf(stderr, "использование: %s --port <порт> --tnum <количество потоков>\n", argv[0]);
+        return 1;
+    }
 
-  return 0;
+    // создание сокета
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        perror("ошибка создания сокета");
+        return 1;
+    }
+
+    // настройка адреса сервера
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(server_fd, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        perror("ошибка привязки сокета");
+        return 1;
+    }
+
+    if (listen(server_fd, 128) < 0) {
+        perror("ошибка прослушивания");
+        return 1;
+    }
+
+    printf("сервер запущен на порту %d\n", port);
+
+    // основной цикл сервера
+    while (true) {
+        struct sockaddr_in client;
+        socklen_t client_len = sizeof(client);
+        int client_fd = accept(server_fd, (struct sockaddr *)&client, &client_len);
+
+        if (client_fd < 0) {
+            perror("ошибка принятия соединения");
+            continue;
+        }
+
+        // получение данных от клиента
+        uint64_t args[3];
+        if (recv(client_fd, args, sizeof(args), 0) <= 0) {
+            perror("ошибка получения данных");
+            close(client_fd);
+            continue;
+        }
+
+        uint64_t begin = args[0];
+        uint64_t end = args[1];
+        uint64_t mod = args[2];
+        printf("получено: %lu %lu %lu\n", begin, end, mod);
+
+        // распределение работы между потоками
+        pthread_t threads[tnum];
+        struct factorial_args fargs[tnum];
+        uint64_t chunk_size = (end - begin + 1) / tnum;
+        uint64_t total = 1;
+
+        for (int i = 0; i < tnum; i++) {
+            fargs[i].begin = begin + i * chunk_size;
+            fargs[i].end = (i == tnum - 1) ? end : (begin + (i + 1) * chunk_size - 1);
+            fargs[i].mod = mod;
+
+            if (pthread_create(&threads[i], NULL, thread_factorial, &fargs[i]) != 0) {
+                perror("ошибка создания потока");
+                close(client_fd);
+                return 1;
+            }
+        }
+
+        // сбор результатов от потоков
+        for (int i = 0; i < tnum; i++) {
+            uint64_t *result;
+            pthread_join(threads[i], (void **)&result);
+            total = MultModulo(total, *result, mod);
+            free(result);
+        }
+
+        printf("результат: %lu\n", total);
+
+        // отправка результата клиенту
+        if (send(client_fd, &total, sizeof(total), 0) <= 0) {
+            perror("ошибка отправки данных");
+        }
+
+        close(client_fd);
+    }
+
+    return 0;
 }
